f213476c5afb39ad65dea4157859cb73995fa0e1
Steven Anderson <sanderson@bpl.org>
2013-02-04 17:32:20 -0500

    Should have fixed incorrect variable issue.

--- lib/omniauth-polaris/adaptor.rb
+++ lib/omniauth-polaris/adaptor.rb
@@ -54,17 +54,10 @@ module OmniAuth
         pin = args[:pin]
         barcode = args[:barcode]
         http_date = Time.now.in_time_zone("GMT").strftime("%a, %d %b %Y %H:%M:%S %Z")
-        puts "here4"
-        puts @http_method
-        puts @config[:http_method]
-        puts @config[:http_uri]
-        puts barcode
 
-        concated_string = @config[:http_method] + @config[:http_uri] + barcode + http_date + pin
+        concated_string = @method + @http_uri + barcode + http_date + pin
         sha1_sig = Base64.strict_encode64("#{OpenSSL::HMAC.digest('sha1',@access_key, concated_string)}")
-        puts "here5"
-        xml_response = RestClient.get http_uri, {'PolarisDate' => http_date, 'Authorization' =>  "PWS " + @config[:access_id] + ":" + sha1_sig}
-        puts "here6"
+        xml_response = RestClient.get http_uri, {'PolarisDate' => http_date, 'Authorization' =>  "PWS " + @access_id + ":" + sha1_sig}
         hashed_response = Hash.from_xml xml_response
 
         hashed_response["PatronValidateResult"]
